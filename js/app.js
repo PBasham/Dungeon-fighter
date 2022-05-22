dd05b88c98275bddfbc5cd241ec0b2580898b0bb
patrickbasham <PatrickTBasham@gmail.com>
2022-05-22 01:19:45 -0400

    added the beginings of boundry collision

--- js/app.js
+++ js/app.js
@@ -11,6 +11,24 @@ let movementArea;
 let winner = false;
 let gameOver = false;
 
+// === *** SET BOUNDRIES *** === //
+// check to see where the player is, adjust the boundries accordingly.
+let bound1;
+let bound2;
+// const bound3 = new Boundry(, , , )
+// const bound4 = new Boundry(, , , )
+// const bound5 = new Boundry(, , , )
+// const bound6 = new Boundry(, , , )
+// const bound7 = new Boundry(, , , )
+// const bound8 = new Boundry(, , , )
+// const bound9 = new Boundry
+// const bound10 = new Boundry
+// const bound11 = new Boundry
+// const bound12 = new Boundry
+// const bound13 = new Boundry
+// const bound14 = new Boundry
+// const bound15 = new Boundry
+// === *** SET BOUNDRIES *** === //
 
 game.setAttribute("height", "608");
 game.setAttribute("width", "1216");
@@ -48,6 +66,19 @@ class Entity {
     }
 }
 
+class Boundry {
+    constructor(x, y, width, height) {
+        this.x = x;
+        this.y = y;
+        this.height = height;
+        this.width = width;
+    }
+    render() {
+        ctx.fillStyle = "grey";
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
+
 // NOTE: set this with canvas or just css / js ? ∆
 // class UserInterface {
 //     constructor(x, y, height, width)
@@ -61,6 +92,9 @@ window.addEventListener("DOMContentLoaded", function(e) {
     orc = new Entity("Orc",608, 192, "darkGreen", 32, 32, 10, 3);
     chest = new Entity("Chest",96, 448, "yellow", 32, 32, 10, 3);
 
+    // set boundries to walls
+    setBoundaries()
+
     const runGame = setInterval(gameLoop, 120);
     
 })
@@ -71,19 +105,26 @@ document.addEventListener("keydown", movementHandler);
 function movementHandler(e) {
     //check if in move state
     if (player.moveState === true) {
+        // check to see if player will collide with a boundry. if so, don't update position.
+            //do that here
+        // else return null
+        // end if
+        if (checkBoundryCollison()) {
+            return null;
+        }
         switch (e.key){
             case "ArrowUp": 
-            player.y > 0 ? (player.y -= moveAmount) : null;
+            player.y -= moveAmount
                 break;
             case "ArrowDown":
-            player.y < game.height - player.height ? player.y += moveAmount : null;
+            player.y += moveAmount
     
                 break;
             case "ArrowLeft": 
-            player.x > 0 ? player.x -= moveAmount : null;
+            player.x -= moveAmount
                 break;
             case "ArrowRight":
-            player.x < game.width - player.width ? player.x += moveAmount : null;
+            player.x += moveAmount
                 break;
         }
     } else {
@@ -91,14 +132,66 @@ function movementHandler(e) {
     }
 }
 
+function setBoundaries() {
+    bound1 = new Boundry(2*32, 3*32, 1*32, 6*32);
+    bound2 = new Boundry(3*32, 2*32, 33*32, 1*32);
+    // const bound3 = new Boundry(, , , )
+    // const bound4 = new Boundry(, , , )
+    // const bound5 = new Boundry(, , , )
+    // const bound6 = new Boundry(, , , )
+    // const bound7 = new Boundry(, , , )
+    // const bound8 = new Boundry(, , , )
+    // const bound9 = new Boundry
+    // const bound10 = new Boundry
+    // const bound11 = new Boundry
+    // const bound12 = new Boundry
+    // const bound13 = new Boundry
+    // const bound14 = new Boundry
+    // const bound15 = new Boundry
+}
+
+function renderBoundries() {
+    bound1.render();
+    bound2.render();
+}
+
+function checkBoundryCollison() {
+    if (
+    checkBoundries(player, bound1) ||
+    checkBoundries(player, bound2)
+    ) {
+        return true;
+    }
+}
+
+function checkBoundries(player, boundry){
+    // boolean to check if a boundry is being collided with
+    let collisionCheck =
+    player.y + player.height > boundry.y &&
+    player.y < boundry.y + boundry.height &&
+    player.x + player.width > boundry.x &&
+    player.x < boundry.x + boundry.width;
+    if(collisionCheck) {
+        console.log("collision detected");
+        return true;
+    }
+}
+
     // run game loop 
 function gameLoop() {
     // clears all entities from screen
     ctx.clearRect(0, 0, game.width, game.height);
+
+    // set boundries
+    renderBoundries()
+
     // adds all entities back if they are alive.
     player.alive === true ? player.render() : null;
     orc.alive === true ? orc.render() : null;
     chest.alive === true ? chest.render() : null;
+
+    // check if the player is touching a boundry
+
 }
 
 // 
